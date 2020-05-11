8c9c674d673913caff4fd65cf98870b9c704100c
screJet <64700446+screJet@users.noreply.github.com>
2020-05-11 22:22:46 +0600

    Add files via upload

---
+++ Журнал Работ/work.py
@@ -0,0 +1,25 @@
+from data.jobs import Jobs
+from flask import Flask, render_template
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/mars_explorer.sqlite")
+
+    @app.route("/")
+    def index():
+        session = db_session.create_session()
+        jobs = session.query(Jobs).all()
+        users = session.query(User).all()
+        names = {name.id: (name.surname, name.name) for name in users}
+        return render_template("index.html", jobs=jobs, names=names)
+
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
